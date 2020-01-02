6b934d3d3af5b497da1665ef25d224009db39bcd
No <m18392283343@163.com>
2020-01-02 16:59:57 +0800

    change test

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-import requests
-r = requests.get('https://www.baidu.com/')
-data = r.text
+# import requests
+# r = requests.get('https://www.baidu.com/')
+# data = r.text
+# print(data)
+#
+#
+#
+data = "('https://www.baidu.com/')"
 print(data)
-
-
-
